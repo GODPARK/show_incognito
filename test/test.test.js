7dc324c557bfd79d5a7d99a835e2750031e75d8f
GODPARK <rootjh937@gmail.com>
2022-08-19 21:50:00 +0900

    project init

---
+++ test/test.test.js
@@ -0,0 +1,24 @@
+
+test('chrome api events', () => {
+    const chrome = require('jest-chrome')
+    const listenerSpy = jest.fn()
+    const sendResponseSpy = jest.fn()
+  
+    chrome.runtime.onMessage.addListener(listenerSpy)
+  
+    expect(listenerSpy).not.toBeCalled()
+    expect(chrome.runtime.onMessage.hasListeners()).toBe(true)
+  
+    chrome.runtime.onMessage.callListeners(
+      { greeting: 'hello' }, // message
+      {}, // MessageSender object
+      sendResponseSpy, // SendResponse function
+    )
+  
+    expect(listenerSpy).toBeCalledWith(
+      { greeting: 'hello' },
+      {},
+      sendResponseSpy,
+    )
+    expect(sendResponseSpy).not.toBeCalled()
+  })
